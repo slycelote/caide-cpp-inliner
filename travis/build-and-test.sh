105cef1e1516253edb966817633910aac92c605f
slycelote <slycelot@yandex.ru>
2016-02-05 22:01:04 -0800

    Fix travis config and script

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -5,15 +5,16 @@ export CXX=g++-4.9
 export CC=gcc-4.9
 
 git submodule init
-git submodule update --depth 1 src/clang
+git submodule update src/clang
 if [ "$CAIDE_USE_SYSTEM_CLANG" = "OFF" ]
 then
-    git submodule update --depth 1 src/llvm
+    git submodule update src/llvm
 fi
 
 mkdir build
 cd build
 cmake -DCAIDE_USE_SYSTEM_CLANG=$CAIDE_USE_SYSTEM_CLANG ../src
-make -j2 cmd test-tool
+make -j2 test-tool
+make cmd
 ctest
 

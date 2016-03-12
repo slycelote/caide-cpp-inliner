c1c2113b2ce37f49151473915a4f2fc637ead1b5
slycelote <slycelot@yandex.ru>
2016-03-12 13:44:37 -0800

    Add appveyor config

---
+++ ci/appveyor-build.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference="Stop"
+
+mkdir build
+cd build
+cmake -G "Visual Studio 14" -DCMAKE_BUILD_TYPE=Release ..\src
+msbuild
+
---
+++ ci/appveyor-test.ps1
@@ -0,0 +1,3 @@
+$ErrorActionPreference="Stop"
+ctest
+

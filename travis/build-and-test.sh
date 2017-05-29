61d5ac2e48728c141c3a457ed42b0bdea10ee2ee
slycelote <slycelot@yandex.ru>
2017-05-28 23:03:44 -0700

    Fix travis build

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -4,20 +4,38 @@ set -ev
 export CXX=g++-4.9
 export CC=gcc-4.9
 
+env
 cmake --version
 "${CXX}" --version
 "${CC}" --version
+date
 
 git submodule init
 git submodule update src/clang
 if [ "$CAIDE_USE_SYSTEM_CLANG" = "OFF" ]
 then
     git submodule update src/llvm
+else
+    # Tell CMake where to look for LLVMConfig
+    case "$CAIDE_CLANG_VERSION" in
+        3.9|4.0)
+            export LLVM_DIR=/usr/lib/llvm-${CAIDE_CLANG_VERSION}/
+            ;;
+
+        *)
+            export LLVM_DIR=/usr/lib/llvm-${CAIDE_CLANG_VERSION}/
+            ;;
+    esac
 fi
+
+date
 
 mkdir build
 cd build
 cmake -DCAIDE_USE_SYSTEM_CLANG=$CAIDE_USE_SYSTEM_CLANG ../src
-make -j2
+make -j2 || make -j1
+
+date
+
 ctest --verbose
 

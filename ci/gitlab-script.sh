6ac43c13a540db3f89b30710efe569e138c433c1
slycelote <slycelot@yandex.ru>
2020-11-08 17:18:02 -0800

    Try GitLab CI

--- ci/gitlab-script.sh
+++ ci/gitlab-script.sh
@@ -2,8 +2,11 @@
 set -ev
 
 apt-get update
-apt-get install -y lsb-release wget software-properties-common
+apt-get install -y lsb-release wget software-properties-common apt-transport-https cmake ninja-build ccache
+
 add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y g++-9 gcc-9
 
 date
 
@@ -20,17 +23,18 @@ then
         *)
             wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
             add-apt-repository "deb http://apt.llvm.org/xenial/   llvm-toolchain-xenial-$CAIDE_CLANG_VERSION  main"
+            apt-get update
             ;;
     esac
 
-    apt-get update
-    apt-get install -y g++-9 gcc-9 ccache clang-"$CAIDE_CLANG_VERSION" libclang-"$CAIDE_CLANG_VERSION"-dev llvm-"$CAIDE_CLANG_VERSION"-dev
+    apt-get install -y clang-"$CAIDE_CLANG_VERSION" libclang-"$CAIDE_CLANG_VERSION"-dev llvm-"$CAIDE_CLANG_VERSION"-dev
 
     export CMAKE_PREFIX_PATH=$Clang_ROOT
 
     # Debug
     llvm-config-"$CAIDE_CLANG_VERSION" --cxxflags --cflags --ldflags --has-rtti
 else
+    apt-get install -y git
     git submodule update --init --depth 1
 fi
 
@@ -45,12 +49,12 @@ date
 
 mkdir build
 cd build
-cmake -DCAIDE_USE_SYSTEM_CLANG=$CAIDE_USE_SYSTEM_CLANG \
+cmake -GNinja -DCAIDE_USE_SYSTEM_CLANG=$CAIDE_USE_SYSTEM_CLANG \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_BUILD_TYPE=MinSizeRel ../src
 
 # First build may run out of memory
-make -j3 || make -j1
+ninja -j3 || ninja -j1
 
 date
 

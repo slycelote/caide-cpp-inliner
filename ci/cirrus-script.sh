a354f80bac355cd01a1835db301779eed40cdb0c
slycelote <slycelot@yandex.ru>
2020-11-09 22:22:37 -0800

    Try Cirrus CI

--- ci/cirrus-script.sh
+++ ci/cirrus-script.sh
@@ -72,7 +72,7 @@ cmake -GNinja -DCAIDE_USE_SYSTEM_CLANG=$CAIDE_USE_SYSTEM_CLANG \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_BUILD_TYPE=MinSizeRel ../src
 
-ninja -j1
+ninja -j2 || ninja -j1
 
 caide_timer
 

f3ce393dabc033678accddde68642a781f7a911a
slycelote <slycelot@yandex.ru>
2020-11-21 14:56:10 -0800

    Try Cirrus CI

--- ci/cirrus-script.sh
+++ ci/cirrus-script.sh
@@ -31,10 +31,10 @@ then
 
 else
     caide_timer
-    pkg install -y gcc9 cmake ninja ccache
+    pkg install -y cmake ninja ccache
     caide_timer
-    export CXX=g++9
-    export CC=gcc9
+    export CXX=clang++
+    export CC=clang
 fi
 
 date

33607d3e6a83ac4bd8a3a8af6e69401b821e8bb9
slycelote <slycelot@yandex.ru>
2021-03-21 18:44:07 -0700

    Add Cirrus CI

--- ci/cirrus-script.sh
+++ ci/cirrus-script.sh
@@ -94,14 +94,12 @@ then
     esac
 else
     ninja install-clang-resource-headers
-    find . -name stdint.h || true
     # The previous target installs builtin clang headers under llvm-project/, but clang libraries expect to find them under lib/
     # (a bug in clang when it's built as a CMake subproject?)
     ln -s $(pwd)/llvm-project/llvm/lib/clang lib/clang
-    find . -name stdint.h || true
 fi
 
-ctest --verbose || true
+ctest --verbose
 
 ci_timer
 
